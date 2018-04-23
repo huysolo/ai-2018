66c6fff1169fbf33a1365f08d76aa56b9dc3ae36
huysolo <32314642+huysolo@users.noreply.github.com>
2018-04-23 19:35:54 +0700

    Revamp All Function

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import copy
+
 # Màn chơi mẫu
 # 1: Vị trí hợp lệ
 # 0: Vị trí không hợp lệ
 # G: Vị trí block không thể đứng thẳng
 # X: Đích
 # LA: Cần gạt nhẹ cho vị trí A
-# LA: Cần gạt nặng cho vị trí A
-# sA: Vị trí thay đổi khi chạm vào cần gạt
-level1 = [['1','1','1','0','0','0','0','0','0','0'],
-          ['1','1','1','1','1','1','0','0','0','0'],
-          ['1','1','1','1','1','1','1','1','1','0'],
-          ['0','1','1','1','G','1','1','1','1','1'],
-          ['0','0','0','0','G','G','1','X','1','1'],
-          ['0','0','0','0','0','0','LK','1','1','0']]
+# HA: Cần gạt nặng cho vị trí A
+# 1A, 0A: Vị trí thay đổi khi chạm vào cần gạt
+level1 = [['1', '1', '1', '0', '0', '0', '0', '0', '0', '0'],
+          ['1', '1', '1', 'LK', '1', '0K', '0', '0', '0', '0'],
+          ['1', '1', '1', '1', '1', '1', '1', '1', '1', '0'],
+          ['1', '1', '1', '1', 'G', '1', '1', '1', '1', '1'],
+          ['0', '0', '0', '0', 'G', 'G', '1', 'X', '1', '1'],
+          ['0', '0', '0', '0', '0', '0', '1', '1', '1', '0']]
+# level1 = [['0','1','1','0','0','0','0','0','0','0'],
+#           ['0','1','1','0','1','0','0','0','0','0'],
+#           ['LK','1','1','1','1','1','1','X','1','0'],
+#           ['0','1','1','1','1','1','1','1','1','1'],
+#           ['0','0','0','0','G','G','1','0','1','1'],
+#           ['0','0','0','0','0','0','1','1','1','0']]
 
 # Order: [row col][row, col]
 b_input = ((2, 1), (2, 2))
@@ -22,7 +29,7 @@ b_input = ((2, 1), (2, 2))
 # 2: Phải
 # -1: Xuống
 # -2: Trái
-avalMove = [1, 2, -1, -2]
+list_direction = [1, 2, -1, -2]
 listMove = []
 
 
@@ -40,6 +47,7 @@ def block_direction(i_input=((0, 0), (0, 0))):
         return -1
     else:
         return 2
+
 
 # Xử lý input với hướng nhận được
 def move(direction, i_input=((0, 0), (0, 0))):
@@ -73,11 +81,11 @@ def move(direction, i_input=((0, 0), (0, 0))):
         elif row == -1 or col == -1:
             return (i_input[0][0] + row, i_input[0][1] + col), (i_input[1][0] + row * 2, i_input[1][1] + col * 2)
 
+
 # Tính giá trị từ input
-def valid_move(i_input=((0, 0), (0, 0)), i_board=None):
+def execute_move(i_input=((0, 0), (0, 0)), i_board=None):
     if i_board is None:
         i_board = []
-
     # Khởi tạo giá trị cho bàn cờ hiện tại
     height = len(i_board)
     width = len(i_board[0])
@@ -94,48 +102,51 @@ def valid_move(i_input=((0, 0), (0, 0)), i_board=None):
             i_state = 'N'
 
         # Trường hợp thay đổi trạng thái gạt cần
-        elif (is_equal(i_input, i_board, 'H') and block_dir == 0) or is_equal([i_input[0]], i_board, 'L') or is_equal([i_input[1]], i_board, 'L'):
-            i_state = 'S'
+        elif (is_equal(i_input, i_board, 'H') and block_dir == 0) or is_equal([i_input[0]], i_board, 'L'):
             i_board = toggle_switch(i_board, get_string(i_input[0], i_board, 1))
-
-        # Trường hợp teraport
+        elif is_equal([i_input[1]], i_board, 'L'):
+            i_board = toggle_switch(i_board, get_string(i_input[1], i_board, 1))
+        # Trường hợp teleport
         elif is_equal([i_input[0]], i_board, 'T') or is_equal([i_input[0]], i_board, 'T'):
             if block_dir != 2:
-                i_state = 'T'
                 if block_dir == 0 or is_equal([i_input[0]], i_board, 'T'):
-                    i_input[0] = teraport(i_board, get_string(i_input[0], i_board, 1))
+                    i_input[0] = teleports(i_board, get_string(i_input[0], i_board, 1))
                 else:
-                    i_input[1] = teraport(i_board, get_string(i_input[0], i_board, 1))
+                    i_input[1] = teleports(i_board, get_string(i_input[0], i_board, 1))
 
         # Trường hợp không hợp lệ
-        else: i_state = 'L'
+        else:
+            i_state = 'L'
 
     # Trường hợp không hợp lệ
-    else: i_state =   'L'
+    else:
+        i_state = 'L'
 
     # Giá trị trả về bao gồm trạng thái bàn cờ, bàn cờ, giá trị input
     return i_state, i_board, i_input
 
 
-def toggle_switch(i_board, str):
+def toggle_switch(i_board, word):
     if i_board is None:
         i_board = []
     for row in range(len(i_board)):
         for col in range(len(i_board[row])):
-            if i_board[row][col][0] == 's' and i_board[row][col][1].upper() == str:
-                if i_board[row][col][1].isupper():
-                    i_board[row][col] =  's' + i_board[row][col][1].lower()
-                else:
-                    i_board[row][col] =  's' + i_board[row][col][1].upper()
+            if len(i_board[row][col]) == 2 and i_board[row][col][1] == word:
+                if i_board[row][col][0] == '0':
+                    i_board[row][col] = '1' + i_board[row][col][1]
+                elif i_board[row][col][0] == '1':
+                    i_board[row][col] = '0' + i_board[row][col][1]
     return i_board
 
-def teraport(i_board, str):
+
+def teleports(i_board, word):
     if i_board is None:
         i_board = []
     for row in range(len(i_board)):
         for col in range(len(i_board[row])):
-            if i_board[row][col][0] == 't' and i_board[row][col][1] == str:
-                return (row, col)
+            if i_board[row][col][0] == 't' and i_board[row][col][1] == word:
+                return row, col
+
 
 def is_not_out_of_range(i_input, height, width):
     rs = True
@@ -143,14 +154,17 @@ def is_not_out_of_range(i_input, height, width):
         rs = rs and i[0] >= 0 and i[1] >= 0 and i[0] < height and i[1] < width
     return rs
 
-def is_equal(i_input, i_board, str, pos = 0):
+
+def is_equal(i_input, i_board, word, pos=0):
     rs = True
     for i in i_input:
-        rs = rs and get_string(i, i_board, pos) == str
+        rs = rs and get_string(i, i_board, pos) == word
     return rs
 
-def get_string(i_block, i_board, pos = 0):
+
+def get_string(i_block, i_board, pos=0):
     return i_board[i_block[0]][i_block[1]][pos]
+
 
 # In bàn cờ
 def print_board(i_board, i_pos=((0, 0), (0, 0))):
@@ -160,42 +174,70 @@ def print_board(i_board, i_pos=((0, 0), (0, 0))):
     for board in n_board:
         print(board)
 
-# Giải thuật dfs
-def dfs(direction, i_pos=((0, 0), (0, 0)), i_board=None):
-    global avalMove
-    global listMove
-    if i_board is None:
-        i_board = []
-    n_input = move(direction, i_pos)
-    state, n_board, n_input = valid_move(n_input, i_board)
-    if state == 'N':
-        for m in list(filter(lambda x: x != -direction, avalMove)):
-            state = dfs(m, n_input, n_board)
-            if state == 'W':
-                break
-    if state == 'W':
-        listMove.append(n_input)
-    return state
+
+# So sách 2 bàn cờ
+def compare_board(i_board1, i_board2):
+    for row in range(len(i_board1)):
+        for col in range(len(i_board2[row])):
+            if i_board1[row][col] != i_board2[row][col]: return False
+    return True
+
+
+def exist(list_input, i_input):
+    for i in list_input:
+        if i == i_input:
+            return True
+    return False
+
+
+def dfs(moves, i_board, state, i_input):
+
+    if state == 'L':
+        return None
+    elif state == 'N':
+        if exist(moves, i_input) and compare_board(i_board, i_board):
+            return None
+        else:
+            n_moves = copy.deepcopy(moves)
+            n_moves.append(i_input)
+            for d in list_direction:
+                n_state, n_board, n_input = execute_move(move(d, i_input), copy.deepcopy(i_board))
+
+                result = dfs(n_moves, n_board, n_state, n_input)
+                if result is not None:
+                    return result
+    else:
+        moves.append(i_input)
+        return moves
+
 
 def bfs():
     return 0
 
-# Phương thức chạy giải thuật
 
-def run(i_method):
-    for mv in list(reversed(listMove)):
+def test():
+    i_input = ((2, 1), (2, 2))
+    global level1
+    while True:
+        direc = input('Insert your method: ')
+        i_input = move(int(direc), i_input)
+        state, level1, i_input = execute_move(i_input, level1)
+        print_board(level1, i_input)
+        print('Block position: ', i_input)
+
+
+# Phương thức chạy giải thuật
+def run(i_inputs, i_board):
+    for i in i_inputs:
         print('-------------')
-        print_board(level1, mv)
-        print('Block position: ', mv)
-    if i_method == 'W':
-        print("Won")
-    else:
-        print('Cannot find available path')
+        state, i_board, n_input = execute_move(i, i_board)
+        print_board(i_board, i)
+        print('Block position: ', i)
 
 
 while True:
     method = input('Insert your method: ')
     if method == 'dfs':
-        run(dfs(0, b_input, level1))
+        run(dfs([], level1, 'N', b_input), level1)
     elif method == 'bfs':
         pass
