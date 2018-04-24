568254a6a6ccf53485478b26987800da55b6958d
huysolo <32314642+huysolo@users.noreply.github.com>
2018-04-24 20:14:57 +0700

    Remove copy board

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ import copy
 # LA: Cần gạt nhẹ cho vị trí A
 # HA: Cần gạt nặng cho vị trí A
 # 1A, 0A: Vị trí thay đổi khi chạm vào cần gạt
-level1 = [['1', '1', '1', '0', '0', '0', '0', '0', '0', '0'],
-          ['1', '1', '1', 'LK', '1', '0K', '0', '0', '0', '0'],
-          ['1', '1', '1', '1', '1', '1', '1', '1', '1', '0'],
-          ['1', '1', '1', '1', 'G', '1', '1', '1', '1', '1'],
-          ['0', '0', '0', '0', 'G', 'G', '1', 'X', '1', '1'],
-          ['0', '0', '0', '0', '0', '0', '1', '1', '1', '0']]
-# level1 = [['0','1','1','0','0','0','0','0','0','0'],
-#           ['0','1','1','0','1','0','0','0','0','0'],
-#           ['LK','1','1','1','1','1','1','X','1','0'],
-#           ['0','1','1','1','1','1','1','1','1','1'],
-#           ['0','0','0','0','G','G','1','0','1','1'],
-#           ['0','0','0','0','0','0','1','1','1','0']]
+# level1 = [['1', '1', '1', '0', '0', '0', '0', '0', '0', '0'],
+#           ['1', '1', '1', 'LA', '1', '0A', '0', '0', '0', '0'],
+#           ['1', '1', '1', '1', '1', '1', '1', '1', '1', '0'],
+#           ['1', '1', '1', '1', 'G', '1', '1', '1', '1', '1'],
+#           ['0', '0', '0', '0', 'G', 'G', '1', 'X', '1', '1'],
+#           ['0', '0', '0', '0', '0', '0', '1', '1', '1', '0']]
+level1 = [['0','1','1','1','0','0','0','0','0','0'],
+          ['0','1','1','1','1','0','0','0','0','0'],
+          ['HK','1','1','1','1','0K','1','X','1','0'],
+          ['0','1','1','1','1','1','1','1','1','1'],
+          ['0','0','0','0','G','G','1','0','1','1'],
+          ['0','0','0','0','0','0','1','1','1','0']]
 
 # Order: [row col][row, col]
 b_input = ((2, 1), (2, 2))
@@ -97,9 +97,9 @@ def execute_move(i_input=((0, 0), (0, 0)), i_board=None):
         if is_equal(i_input, i_board, 'X'):
             i_state = 'W'
 
-        # Trường hợp không thay đổi trạng thái
-        elif is_equal(i_input, i_board, '1') or (block_dir != 0 and is_equal(i_input, i_board, 'G')):
-            i_state = 'N'
+        # Trường hợp thua
+        elif is_equal([i_input[0]], i_board, '0') or is_equal([i_input[1]], i_board, '0') or (block_dir == 0 and is_equal(i_input, i_board, 'G')):
+            i_state = 'L'
 
         # Trường hợp thay đổi trạng thái gạt cần
         elif (is_equal(i_input, i_board, 'H') and block_dir == 0) or is_equal([i_input[0]], i_board, 'L'):
@@ -113,10 +113,6 @@ def execute_move(i_input=((0, 0), (0, 0)), i_board=None):
                     i_input[0] = teleports(i_board, get_string(i_input[0], i_board, 1))
                 else:
                     i_input[1] = teleports(i_board, get_string(i_input[0], i_board, 1))
-
-        # Trường hợp không hợp lệ
-        else:
-            i_state = 'L'
 
     # Trường hợp không hợp lệ
     else:
@@ -168,11 +164,20 @@ def get_string(i_block, i_board, pos=0):
 
 # In bàn cờ
 def print_board(i_board, i_pos=((0, 0), (0, 0))):
-    n_board = copy.deepcopy(i_board)
-    n_board[i_pos[0][0]][i_pos[0][1]] = 'B'
-    n_board[i_pos[1][0]][i_pos[1][1]] = 'B'
-    for board in n_board:
-        print(board)
+    i_board[i_pos[0][0]][i_pos[0][1]] = 'B '
+    i_board[i_pos[1][0]][i_pos[1][1]] =  'B '
+    for board in i_board:
+        for word in board:
+          if word[0] == '0':
+            word = '0 '
+          elif word[0] == '1':
+            word = '1 '
+          elif word == 'X':
+            word = 'X '
+          elif len(word) == 1:
+            word = word.ljust(2)
+          print(word + ' ', sep=' ', end='', flush=True)
+        print('\n')
 
 
 # So sách 2 bàn cờ
@@ -183,32 +188,31 @@ def compare_board(i_board1, i_board2):
     return True
 
 
-def exist(list_input, i_input):
-    for i in list_input:
-        if i == i_input:
+def exist(moves_w_board, i_input, i_board):
+    for i in moves_w_board:
+        if i[0] == i_input and compare_board(i[1], i_board):
             return True
     return False
 
-
-def dfs(moves, i_board, state, i_input):
+def dfs(moves_w_board, i_board, state, i_input):
 
     if state == 'L':
         return None
     elif state == 'N':
-        if exist(moves, i_input) and compare_board(i_board, i_board):
+        if exist(moves_w_board, i_input, i_board):
             return None
         else:
-            n_moves = copy.deepcopy(moves)
-            n_moves.append(i_input)
+            n_moves_w_board = moves_w_board
+            n_moves_w_board.append((i_input, i_board))
             for d in list_direction:
                 n_state, n_board, n_input = execute_move(move(d, i_input), copy.deepcopy(i_board))
 
-                result = dfs(n_moves, n_board, n_state, n_input)
+                result = dfs(n_moves_w_board, n_board, n_state, n_input)
                 if result is not None:
                     return result
     else:
-        moves.append(i_input)
-        return moves
+        moves_w_board.append((i_input, i_board))
+        return moves_w_board
 
 
 def bfs():
@@ -219,25 +223,29 @@ def test():
     i_input = ((2, 1), (2, 2))
     global level1
     while True:
-        direc = input('Insert your method: ')
-        i_input = move(int(direc), i_input)
+        direct = input('Insert your method: ')
+        i_input = move(int(direct), i_input)
         state, level1, i_input = execute_move(i_input, level1)
         print_board(level1, i_input)
         print('Block position: ', i_input)
 
 
 # Phương thức chạy giải thuật
-def run(i_inputs, i_board):
-    for i in i_inputs:
-        print('-------------')
-        state, i_board, n_input = execute_move(i, i_board)
-        print_board(i_board, i)
-        print('Block position: ', i)
+def run(i_inputs):
+    if i_inputs is not None:
+        for i in i_inputs:
+            print('-------------')
+            # state, i_board, n_input = execute_move(i, i_board)
+            print_board(i[1], i[0])
+            print('Block position: ', i[0])
+        print('The path has been found')
+    else:
+        print('Cannot find the path')
 
 
 while True:
     method = input('Insert your method: ')
     if method == 'dfs':
-        run(dfs([], level1, 'N', b_input), level1)
+        run(dfs([], level1.copy(), 'N', b_input))
     elif method == 'bfs':
         pass
